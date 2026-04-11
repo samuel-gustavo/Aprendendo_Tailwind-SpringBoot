37222542290f3ae1279344acf13a023c4c2d4ed0
Samuel Gustavo <dev.samuelgustavo@gmail.com>
2026-04-11 07:54:28 -0300

    Realizada aulas 3, 4 e 5 de Tailwind

--- src/main/java/com/devsamuel/tailwindaprendizados/controllers/AulasController.java
+++ src/main/java/com/devsamuel/tailwindaprendizados/controllers/AulasController.java
@@ -9,16 +9,26 @@ public class AulasController {
 
 	@GetMapping("/aula1")
 	public String aula1() {
-		return "aula1";
+		return "aula1_teste";
 	}
 	
 	@GetMapping("/aula2")
 	public String aula2() {
-		return "aula2";
+		return "aula2_fontes-e-cores";
 	}
 	
 	@GetMapping("/aula3")
 	public String aula3() {
-		return "aula3";
+		return "aula3_espacamentos";
+	}
+	
+	@GetMapping("/aula4")
+	public String aula4() {
+		return "aula4_bordas";
+	}
+	
+	@GetMapping("/aula5")
+	public String aula5() {
+		return "aula5_background";
 	}
 }
